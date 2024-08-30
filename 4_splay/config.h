32505c844cca00d704f3a876c595e140a1ff0707
Adam Tombleson <rekarnar@gmail.com>
2024-08-30 15:50:42 +0300

    feat: starting on 4

---
+++ 4_splay/config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define MASTER_LEFT
+
+// key matrix size
+// #define MATRIX_ROWS 4
+// #define MATRIX_COLS 12
+
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN D1
+#define SPLIT_USB_DETECT
+
+#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
+
+#define DIODE_DIRECTION COL2ROW
+
+#define DEBOUNCE 5
+
+// Vial features:
+#define VIAL_TAP_DANCE_ENTRIES 25
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+#define VIAL_KEY_OVERRIDE_ENTRIES 2
+#define VIAL_COMBO_ENTRIES 2
---
+++ 4_splay/keymaps/vial/rules.mk
@@ -0,0 +1,10 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+
+# LTO makes the compiler work harder when optimizing your code,
+# resulting in a smaller firmware size.
+LTO_ENABLE = yes
+
+SPLIT_KEYBOARD = yes
+# CONSOLE_ENABLE = yes
