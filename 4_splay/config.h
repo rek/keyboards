b93fd7d83a932e44e6e053f930ec54a5cde30af7
Adam Tombleson <rekarnar@gmail.com>
2024-10-27 21:03:05 +0300

    feat: closer to final splay config

--- 4_splay/config.h
+++ 4_splay/config.h
@@ -2,16 +2,12 @@
 
 #define MASTER_LEFT
 
-// key matrix size
-// #define MATRIX_ROWS 4
-// #define MATRIX_COLS 12
-
 #define USE_SERIAL
 #define SOFT_SERIAL_PIN D1
 #define SPLIT_USB_DETECT
 
-#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
+#define MATRIX_ROW_PINS { F4, F5, F6, F7 }
+#define MATRIX_COL_PINS { D0, D4, C6, D7, E6, B4 }
 
 #define DIODE_DIRECTION COL2ROW
 
